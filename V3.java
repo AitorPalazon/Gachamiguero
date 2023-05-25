1084ddddaa9075c4bf3b622546812a54ba282029
Aitor Palazon <aitpalmon@alu.edu.gva.es>
2023-05-25 18:15:31 +0200

    Código de V3 creado

--- V3.java
+++ V3.java
@@ -1,3 +1,21 @@
 public class V3 {
-    
+    public static void main(String[] args) {
+        V3 programa = new V3();
+        programa.start();
+    }
+
+    void start(){
+
+    }
+
+    public static String catar(int n){
+        if ((n%3==0)&&(n%5==0))
+            return "Gachamiga";
+        else if (n%5==0) {
+            return "Miga";
+        }else if (n%3==0) {
+            return "Gacha";
+        }else
+            return n+"";
+    }
 }
