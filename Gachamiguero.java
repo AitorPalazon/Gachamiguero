f445a1243a03117e01882cd00da4f9d53f8da827
Aitor Palazon <aitpalmon@alu.edu.gva.es>
2023-05-25 18:20:02 +0200

    Código del programa gachamiguero

--- Gachamiguero.java
+++ Gachamiguero.java
@@ -1,3 +1,21 @@
 public class Gachamiguero {
-    
+    public static void main(String[] args) {
+        Gachamiguero programa = new Gachamiguero();
+        programa.start();
+    }
+
+    void start(){
+
+    }
+
+    public static String catar(int n){
+        if ((n%3==0)&&(n%5==0))
+            return "Gachamiga";
+        else if (n%5==0) {
+            return "Miga";
+        }else if (n%3==0) {
+            return "Gacha";
+        }else
+            return n+"";
+    }
 }
