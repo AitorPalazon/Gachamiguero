5db4a4077beb02f74163ab327064072da0c91425
Aitor Palazon <aitpalmon@alu.edu.gva.es>
2023-05-25 18:15:01 +0200

    Todos los tests acabados de V3

--- TestV3.java
+++ TestV3.java
@@ -12,4 +12,74 @@ public class TestV3 {
         String result = V3.catar(2);
         Assertions.assertEquals("2",result);
     }
+    @Test
+    public void Test3(){
+        String result = V3.catar(3);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void Test4(){
+        String result = V3.catar(4);
+        Assertions.assertEquals("4",result);
+    }
+    @Test
+    public void Test5(){
+        String result = V3.catar(5);
+        Assertions.assertEquals("Miga",result);
+    }
+    @Test
+    public void Test6(){
+        String result = V3.catar(6);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void Test7(){
+        String result = V3.catar(7);
+        Assertions.assertEquals("7",result);
+    }
+    @Test
+    public void Test8(){
+        String result = V3.catar(8);
+        Assertions.assertEquals("8",result);
+    }
+    @Test
+    public void Test9(){
+        String result = V3.catar(9);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void Test10(){
+        String result = V3.catar(10);
+        Assertions.assertEquals("Miga",result);
+    }
+    @Test
+    public void Test11(){
+        String result = V3.catar(11);
+        Assertions.assertEquals("11",result);
+    }
+    @Test
+    public void Test12(){
+        String result = V3.catar(12);
+        Assertions.assertEquals("Gacha",result);
+    }
+    @Test
+    public void Test13(){
+        String result = V3.catar(13);
+        Assertions.assertEquals("13",result);
+    }
+    @Test
+    public void Test14(){
+        String result = V3.catar(14);
+        Assertions.assertEquals("14",result);
+    }
+    @Test
+    public void Test15(){
+        String result = V3.catar(15);
+        Assertions.assertEquals("Gachamiga",result);
+    }
+    @Test
+    public void Test30(){
+        String result = V3.catar(30);
+        Assertions.assertEquals("Gachamiga",result);
+    }
 }
