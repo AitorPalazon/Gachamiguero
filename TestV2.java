d9f84bd0175298d98c3e40f37d06987a2c4027b9
Aitor Palazon <aitpalmon@alu.edu.gva.es>
2023-05-25 18:06:51 +0200

    Test 1

--- TestV2.java
+++ TestV2.java
@@ -1,3 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestV2 {
-    
+    @Test
+    public void Test1(){
+        String result = V2.cocinar(1);
+        Assertions.assertEquals("1",result);
+    }
 }
