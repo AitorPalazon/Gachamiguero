7368db81c31fa8aa34514fade80b6cd657f6074e
Aitor Palazon <aitpalmon@alu.edu.gva.es>
2023-05-25 18:18:51 +0200

    Test 1, V4

--- TestGachamiguero.java
+++ TestGachamiguero.java
@@ -1,3 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class TestGachamiguero {
-    
+    @Test
+    public void Test1(){
+        String result = Gachamiguero.catar(1);
+        Assertions.assertEquals("1",result);
+    }
 }
